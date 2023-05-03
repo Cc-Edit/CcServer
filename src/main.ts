92eb5afb1b47f56825abeb5c4a115d160bd7300f
ccedit <ccedit@126.com>
2023-05-03 22:55:38 +0800

    修改端口

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, {
+    abortOnError: false, // 阻止程序异常退出
+  });
+  await app.listen(8080);
 }
 bootstrap();
