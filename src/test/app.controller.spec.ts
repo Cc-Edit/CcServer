a8816c637993d5a1cf310d08864744c1332b7a00
ccedit <ccedit@126.com>
2023-05-04 19:42:44 +0800

    目录结构调整

--- src/test/app.controller.spec.ts
+++ src/test/app.controller.spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppController } from '../controller/app';
+import { AppService } from '../service/app';
 
 describe('AppController', () => {
   let appController: AppController;
