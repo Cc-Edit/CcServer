721d0b642226a84d748187ec7aee5a7bd326a632
ccedit <ccedit@126.com>
2023-05-05 18:46:10 +0800

    logger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
+import { LoggerMiddleware } from './lib/middleware/logger.middleware';
 import { ApplicationModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(ApplicationModule, {
     abortOnError: false, // 阻止程序异常退出
   });
+  app.use(LoggerMiddleware); // 全局 logger
   await app.listen(8080);
 }
 bootstrap();
