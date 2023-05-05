c53a3fbeb3beaa6311d9ce5ce6e0741a52bd1785
ccedit <ccedit@126.com>
2023-05-05 20:26:47 +0800

    pipe

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { LoggerMiddleware } from './lib/middleware/logger.middleware';
+import { AuthGuard } from './lib/guard/auth.guard';
 import { ApplicationModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(ApplicationModule, {
     abortOnError: false, // 阻止程序异常退出
   });
+
   app.use(LoggerMiddleware); // 全局 logger
+  app.useGlobalGuards(new AuthGuard()); // 全局路由守卫
+
   await app.listen(8080);
 }
 bootstrap();
