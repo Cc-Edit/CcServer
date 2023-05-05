0694ae687887e0264e3f0ae59941e83131268d3b
ccedit <ccedit@126.com>
2023-05-05 19:06:59 +0800

    pipe

--- src/user/dto/user.ts
+++ src/user/dto/user.ts
@@ -1,6 +1,15 @@
+import { IsString, IsInt } from 'class-validator';
+
 export class UserDto {
+  @IsString()
   readonly name: string;
+
+  @IsInt()
   readonly age: number;
+
+  @IsInt()
   readonly phone: number;
+
+  @IsString()
   readonly password: string;
 }
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -6,17 +6,20 @@ import {
   Put,
   Param,
   Body,
+  UsePipes,
   NotFoundException,
 } from '@nestjs/common';
 import { UserDto } from './dto/user';
 import { UserService } from './user.service';
 import { User } from './interfaces/user';
+import { ValidationPipe } from './pipe/validate.pipe';
 
 @Controller('user')
 export class UserController {
   constructor(private userService: UserService) {}
 
   @Post()
+  @UsePipes(ValidationPipe)
   async create(@Body() user: UserDto) {
     this.userService.create(user);
   }
