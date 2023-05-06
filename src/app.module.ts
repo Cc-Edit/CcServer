80807032a853880ddfa2dd49085660a3922bc893
ccedit <ccedit@126.com>
2023-05-07 00:27:29 +0800

    增加mongodb以来

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
-import { UserModule } from './user/user.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
 import { AuthService } from './auth/auth.service';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UserModule, AuthModule],
+  imports: [
+    UsersModule,
+    AuthModule,
+    MongooseModule.forRoot('mongodb://127.0.0.1:27017/CcServer'),
+  ],
   providers: [AuthService],
 })
 export class ApplicationModule {}
