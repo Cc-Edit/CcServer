2f885a6c9b846d4418abff0e06f25ce01b0cbebb
ccedit <ccedit@126.com>
2023-05-08 15:18:49 +0800

    接口校验

--- src/users/dto/user.ts
+++ src/users/dto/user.ts
@@ -1,7 +1,22 @@
-import { IsString, IsInt } from 'class-validator';
+import {
+  IsString,
+  IsNotEmpty,
+  Length,
+  IsInt,
+  Matches,
+} from 'class-validator';
 
 export class UserDto {
   @IsString()
+  @IsNotEmpty({
+    message: '用户名不能为空',
+  })
+  @Length(5, 20, {
+    message: '用户名需要5-20个字符',
+  })
+  @Matches(/^[a-zA-Z0-9_]/, {
+    message: '用户名只能包含字母、数字、下划线',
+  })
   readonly name: string;
 
   @IsInt()
--- src/users/pipe/validate.pipe.ts
+++ src/users/pipe/validate.pipe.ts
@@ -5,24 +5,25 @@ import {
   BadRequestException,
 } from '@nestjs/common';
 import { validate } from 'class-validator';
-import { plainToClass } from 'class-transformer';
+import { plainToInstance } from 'class-transformer';
 
 @Injectable()
 export class ValidationPipe implements PipeTransform<any> {
   async transform(value: any, { metatype }: ArgumentMetadata) {
+    // 类型校验，非js类型直接返回
     if (!metatype || !this.toValidate(metatype)) {
       return value;
     }
-    const object = plainToClass(metatype, value);
+    const object = plainToInstance(metatype, value);
     const errors = await validate(object);
     if (errors.length > 0) {
-      throw new BadRequestException('Validation failed');
+      throw new BadRequestException(JSON.stringify(errors));
     }
     return value;
   }
 
-  private toValidate(metatype: Function): boolean {
-    const types: Function[] = [String, Boolean, Number, Array, Object];
-    return !types.includes(metatype);
+  private toValidate(metaType: unknown): boolean {
+    const types: unknown[] = [String, Boolean, Number, Array, Object];
+    return !types.includes(metaType);
   }
 }
