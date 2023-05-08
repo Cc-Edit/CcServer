286d8741506a6ceba128dc37cca42b3e407eef61
ccedit <ccedit@126.com>
2023-05-08 23:54:44 +0800

    User表设计

--- src/users/dto/user.ts
+++ src/users/dto/user.ts
@@ -1,10 +1,5 @@
-import {
-  IsString,
-  IsNotEmpty,
-  Length,
-  IsInt,
-  Matches,
-} from 'class-validator';
+import { IsString, IsNotEmpty, Length, IsInt, Matches, IsNumber } from 'class-validator';
+import { UserStatus } from '../schemas/user.schema';
 
 export class UserDto {
   @IsString()
@@ -19,12 +14,50 @@ export class UserDto {
   })
   readonly name: string;
 
-  @IsInt()
-  readonly age: number;
-
-  @IsInt()
-  readonly phone: number;
+  @IsString()
+  @IsNotEmpty({
+    message: '手机号不能为空',
+  })
+  @Matches(/^((13[0-9])|(14[5|7])|(15([0-3]|[5-9]))|(18[0,5-9]))\\d{8}$/, {
+    message: '手机号码不正确',
+  })
+  readonly phone: string;
 
   @IsString()
+  @Matches(
+    /^([A-Za-z0-9_\\-\\.])+\\@([A-Za-z0-9_\\-\\.])+\\.([A-Za-z]{2,4})$/,
+    {
+      message: '邮箱地址格式不正确',
+    },
+  )
+  readonly email: string;
+
+  @IsString()
+  readonly avatar: string;
+
+  @IsString()
+  @Matches(
+    /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[._~!@#$^&*])[A-Za-z0-9._~!@#$^&*]{6,}$/,
+    {
+      message: '密码中必须包含字母、数字和特殊字符, 长度大于6位',
+    },
+  )
   readonly password: string;
+
+  @IsString()
+  readonly salt: string;
+
+  @IsString()
+  readonly uuid: string;
+
+  @IsNumber()
+  readonly createDate: string;
+
+  @IsNumber()
+  readonly updateDate: string;
+
+  @IsNumber()
+  readonly role: string;
+
+  readonly status: typeof UserStatus;
 }
--- src/users/schemas/user.schema.ts
+++ src/users/schemas/user.schema.ts
@@ -2,17 +2,47 @@ import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
 
 export type UserDocument = User & Document;
+export enum UserStatus {
+  Delete,
+  Disable,
+  Expired,
+  Open,
+}
 
 @Schema()
 export class User extends Document {
-  @Prop()
-  name: string;
+  @Prop({ required: true })
+  name: string; // 用户名
 
   @Prop({ required: true })
-  age: number;
+  phone: string; // 手机号
 
   @Prop()
-  breed: string;
+  email: string; // 邮箱
+
+  @Prop()
+  avatar: string; // 头像
+
+  @Prop({ required: true })
+  password: string; // 密码
+
+  @Prop({ required: true })
+  salt: string; // 盐
+
+  @Prop({ required: true })
+  uuid: string; // 用户id
+
+  @Prop({ required: true })
+  createDate: number; // 创建时间
+
+  @Prop({ required: true })
+  updateDate: number; // 最后更新时间
+
+  @Prop({ required: true })
+  role: number; // 角色
+
+  @Prop({ required: true })
+  status: typeof UserStatus; // 用户状态
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
