39afb7fb65c08c8ff5da239ab301f2afcfb44b0d
ccedit <ccedit@126.com>
2023-05-27 00:14:00 +0800

    jwt 完成

--- src/auth/dto/login-user.dto.ts
+++ src/auth/dto/login-user.dto.ts
@@ -1,11 +1,11 @@
-import { IsString, IsNotEmpty } from 'class-validator'
+import { IsString, IsNotEmpty } from 'class-validator';
 
 export class LoginUser {
   @IsString({ message: '类型错误' })
   @IsNotEmpty({ message: '账号不能为空' })
-  readonly username: string
+  readonly username: string;
 
   @IsString({ message: '类型错误' })
   @IsNotEmpty({ message: '密码不能为空' })
-  readonly password: string
+  readonly password: string;
 }
