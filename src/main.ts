b2d8d2e434f943daf5a0e609319efa73f4a0a148
ccedit <ccedit@126.com>
2023-05-27 23:10:33 +0800

    安全

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import helmet from 'helmet'
 import { LoggerMiddleware } from './lib/middleware/logger.middleware';
 import { AuthGuard } from './lib/guard/auth.guard';
 import { ApplicationModule } from './app.module';
@@ -10,6 +11,14 @@ async function bootstrap() {
     abortOnError: false, // 阻止程序异常退出
     logger: ['log', 'error', 'warn', 'debug'],
   });
+
+  // 设置 api 访问前缀
+  app.setGlobalPrefix(AppConfig.Base.APP.prefix)
+
+  // web 安全，防常见漏洞
+  // 注意： 开发环境如果开启 nest static module 需要将 crossOriginResourcePolicy 设置为 false 否则 静态资源 跨域不可访问
+  // { crossOriginOpenerPolicy: { policy: 'same-origin-allow-popups' }, crossOriginResourcePolicy: false }
+  app.use(helmet({ crossOriginOpenerPolicy: { policy: 'same-origin-allow-popups' }, crossOriginResourcePolicy: false }))
 
   app.use(LoggerMiddleware); // 全局 logger
   app.useGlobalGuards(new AuthGuard()); // 全局路由守卫
