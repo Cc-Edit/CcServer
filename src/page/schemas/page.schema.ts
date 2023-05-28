c6573736040500d0398b902bb097bcb92bd88076
ccedit <ccedit@126.com>
2023-05-28 23:28:39 +0800

    页面增删改查

--- src/page/schemas/page.schema.ts
+++ src/page/schemas/page.schema.ts
@@ -1,6 +1,5 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
-import { User } from '../../user/schemas/user.schema';
 
 export type PageDocument = Page & Document;
 
