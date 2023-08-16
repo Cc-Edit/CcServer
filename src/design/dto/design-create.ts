38c4e7ad77c9a4db5525c6079a179ef539eabf71
ccedit <ccedit@126.com>
2023-08-16 21:21:42 +0800

    design 接口

--- src/design/dto/design-create.ts
+++ src/design/dto/design-create.ts
@@ -17,36 +17,21 @@ export class DesignCreate {
 
   @ApiProperty({ description: 'layout 结构json', required: true })
   @IsString({ message: '类型错误' })
-  @IsNotEmpty({
-    message: 'layout不能为空',
-  })
   layout: string;
 
   @ApiProperty({ description: 'attribute 结构json', required: true })
   @IsString({ message: '类型错误' })
-  @IsNotEmpty({
-    message: 'attribute 不能为空',
-  })
   attribute: string;
 
   @ApiProperty({ description: 'event 结构json', required: true })
   @IsString({ message: '类型错误' })
-  @IsNotEmpty({
-    message: 'event不能为空',
-  })
   event: string;
 
   @ApiProperty({ description: 'api 结构json', required: true })
   @IsString({ message: '类型错误' })
-  @IsNotEmpty({
-    message: 'api不能为空',
-  })
   api: string;
 
   @ApiProperty({ description: 'page 结构json', required: true })
   @IsString({ message: '类型错误' })
-  @IsNotEmpty({
-    message: 'page不能为空',
-  })
   page: string;
 }
