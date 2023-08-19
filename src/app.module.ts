90700ebc9429bd8cd83156f2762136e12da0fc50
ccedit <ccedit@126.com>
2023-08-19 23:44:59 +0800

    oss 接口

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ServeStaticModule } from '@nestjs/serve-static';
 import { UserModule } from './user/user.module';
 import { DesignModule } from './design/design.module';
 import { AuthModule } from './auth/auth.module';
@@ -9,11 +10,13 @@ import { APP_GUARD } from '@nestjs/core';
 import { AppConfig } from '../config/app.config';
 import { ScheduleModule } from '@nestjs/schedule';
 import { TaskModule } from './task/task.module';
-
 import { JwtAuthGuard } from './common/guards/auth.guard';
-
+import { join } from 'path';
 @Module({
   imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '../../', 'oss'),
+    }),
     ScheduleModule.forRoot(),
     MongooseModule.forRoot(AppConfig.DB.url + '/ccServer?authSource=admin', {
       connectionName: 'ccServer',
